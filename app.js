55026389bef6cc94da77293e67221201653499e7
Luchanso <luchansodev@gmail.com>
2016-10-25 16:38:52 +0300

    Fix micro bug

--- app.js
+++ app.js
@@ -12,10 +12,6 @@ process.env.PORT = process.env.PORT || 5000
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-let formData = {
-
-}
-
 app.get('/', (req, res) => {
   res.send('Hello! <a href="/auth">Authorize</a>')
   // request.post('https://www.livecoding.tv:443/api/user/followers/', {formData}, (err, resp, body) => {
